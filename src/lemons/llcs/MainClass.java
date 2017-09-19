05303709dc2ff0f807fbcf6d26fbba8269c35c04
mrlemons <32108038+mrlemons@users.noreply.github.com>
2017-09-19 14:34:49 -0400

    First

---
+++ src/lemons/llcs/MainClass.java
@@ -0,0 +1,50 @@
+/*
+ * Variable Name    Datatype	Variable description
+ * ----------------------------------------------------------------
+ * userName			String		The users name
+ * firstNumber		int			First number user wants to calculate
+ * secondNumber		int			Second number the user wants to calculate*
+ * total			int			Final calculated number
+ * */
+
+package lemons.llcs;
+
+import java.util.Scanner;
+
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+		String userName;
+		int firstNumber;
+		int secondNumber;
+		int total;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, what is your name?");
+		
+		userName = input.nextLine();
+		
+		System.out.println("Hello "+ userName);
+		
+		System.out.println("Please enter your first number.");
+		
+		firstNumber = input.nextInt();
+		
+		System.out.println("Your number is " + firstNumber);
+		
+		System.out.println("Please enter your second number");
+		
+		secondNumber = input.nextInt();
+		
+		System.out.println("Your second number is " + secondNumber);
+		
+		total = firstNumber + secondNumber;
+		
+		System.out.println(userName + " your total is: " + firstNumber + "+" + secondNumber + "=" + total );
+
+	}
+
+}
